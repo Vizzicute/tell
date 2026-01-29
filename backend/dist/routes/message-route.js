b7f010458cfef347ba6d549fc58eb585c037e08d
Vic_Octavian <victoctavian124@gmail.com>
2026-01-29 14:40:23 +0100

    change to prisma client

--- backend/dist/routes/message-route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../middleware/auth");
-const message_controller_1 = require("../controllers/message-controller");
-const router = (0, express_1.Router)();
-router.get("/chat/:chatId", auth_1.protectRoute, message_controller_1.getMessages);
-exports.default = router;
-//# sourceMappingURL=message-route.js.map
